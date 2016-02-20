ed44d3136661ffcd188e0ec177fa4f6b0f194642
Hazymoon <tnx.hazymoon@gmail.com>
2016-02-20 17:23:58 +0900

    セクションの設計を見直し

--- neurocrowd/sections.lua
+++ neurocrowd/sections.lua
@@ -7,70 +7,58 @@
 
 local utf8 = require('lua-utf8')
 
-------
--- 階層レベルオブジェクト
--- @param aMark 階層レベルマーク（root、■、◆、●、▼のいずれか）
--- @section Level
-local function Level(aMark)
-    local self = {}
-    local mark = aMark
-    
-    ------
-    -- 階層レベルの取得
-    -- @return 階層レベル（0～4）、不正な場合は-1を返す
-    function self.is()
-        if mark == 'root' then
-            return 0
-        elseif mark == '■' then
-            return  1
-        elseif mark == '◆' then
-            return 2
-        elseif mark == '●' then
-            return 3
-        elseif mark == '▼' then
-            return 4
-        else
-            return -1
-        end
+local function levelIs(line)
+    local mark = utf8.match(line, "^([■◆●▼]+).*$")
+    if line == 'root' then
+        return 0
+    elseif mark == '■' then
+        return 1
+    elseif mark == '◆' then
+        return 2
+    elseif mark == '●' then
+        return 3
+    elseif mark == '▼' then
+        return 4
+    else
+        return -1
     end
-
-    ----
-    -- 階層レベルの比較
-    -- @param other 他セクションの階層レベルオブジェクト
-    -- @return -1:otherの方が上の階層、0:同レベル、1:otherの方が下の階層
-    function self.compareTo(other)
-        local result = other.is() - self.is()
-        if result == 0 then
-            return 0
-        elseif result < 0 then
-            return -1
-        else
-            return 1
-        end
-    end
-
-    return self
 end
 
----- 親子関係オブジェクト
--- @param aParent 親
--- @return 親子関係オブジェクト
-local function Relation(aParent)
+---- セクションオブジェクト
+-- @param line セクションの元となる行
+-- @param aParent 親セクション
+-- @section Section
+local function Section(line, aParent)
     local self = {}
+
+    local level = levelIs(line)
+    local sectionName = line
     local parent = aParent
     local children = {}
+    local sentences = {}
 
-    ---- 子の追加
-    -- @param child 追加する子
+    --- 子セクションの追加
+    -- @param child 子セクション
     function self.addChild(child)
         table.insert(children, child)
     end
 
-    ---- 親の取得
-    -- @return 親
-    function self.parentIs()
-        -- functionを親としてインスタンスを作ったのち、このメソッドで返すと別インスタンスになる
-        return parent
+    --- セクション内文章の追加
+    -- @param aLine 追加する文章
+    function self.append(aLine)
+        assert(type(aLine) == "string")
+        table.insert(sentences, aLine)
+    end
+
+    function self.levelIs()
+        return level
+    end
+
+    ---- セクション同士の階層の比較
+    -- @param target 比較対象のセクション
+    -- @return 負数:自セクションが下階層、0:同階層、正数:自セクションが上階層
+    function self.compareTo(target)
+        return target.levelIs() - level
     end
 
     ---- 子のイテレータ取得
@@ -88,75 +76,13 @@ local function Relation(aParent)
         end)
     end
 
-    return self
-end
-
----- セクションオブジェクト
--- @param line セクションの元となる行
--- @param aParent 親セクション
--- @section Section
-local function Section(line, aParent)
-    local self = {}
-
-    -- fields
-    local levelMark = utf8.match(line, "^([■◆●▼]+).*$")
-    local level = Level(levelMark)
-    local sectionName = utf8.match(line, "^[■◆●▼](.*)$")
-    local facts = {}
-    local relation = Relation(aParent)
-
-    --- 子セクションの追加
-    -- @param aSection 子セクション
-    function self.addChildren(aSection)
-        relation.addChildren(aSection)
+    if parent ~= nil then
+        parent.addChild(self)
     end
-
-    --- セクションレベルの取得
-    -- @return セクションレベル
-    function self.levelIs()
-        return level.is()
-    end
-
-    --- セクション名の取得
-    -- @return セクション名
-    function self.toString()
-        return levelMark .. sectionName 
-    end
-
-    --- セクション内文章の追加
-    -- @param aLine 追加する文章
-    function self.append(aLine)
-        assert(type(aLine) == "string")
-        table.insert(facts, aLine)
-    end
-
-    ---- セクション同士の階層の比較
-    -- @param target 比較対象のセクション
-    -- @return -1:自セクションが下階層、0:同階層、1:自セクションが上階層
-    function self.compareTo(target)
-        local result = target.levelIs() - self.levelIs()
-        if result < 0 then
-            return -1
-        elseif result == 0 then
-            return 0
-        else
-            return 1
-        end
-    end
-
-    ---- 子を数え上げる
-    -- @return 子を数え上げるイテレータ
-    function self.iterateChildren()
-        return relation.iterateChildren()
-    end
-
-    aParent.addChildren(self)
 
     return self
 end
 
 return {
-    Level = Level,
-    Relation = Relation,
     Section = Section,
 }
--- neurocrowd/test_sections.lua
+++ neurocrowd/test_sections.lua
@@ -4,89 +4,49 @@
 local luaunit = require('luaunit')
 local utf8 = require('lua-utf8')
 local sections = require('sections')
-local level = sections.Level
-local Relation = sections.Relation
+local Section = sections.Section
 
-TestLevel = {}
+---- テストスイート：Section
+TestSection = {}
 
-----
--- 階層レベルオブジェクトの初期化のテスト(root階層の時)
-function TestLevel:test_initialize_root()
-    local obj_level0 = level('root')
-    luaunit.assertEquals(obj_level0.is(), 0)
+---- インスタンス生成
+function TestSection:testNew()
+    local section = Section('■セクション')
+    luaunit.assertEquals(section.levelIs(), 1)
 end
 
-----
--- 階層レベルオブジェクトの初期化のテスト(第1階層の時)
-function TestLevel:test_initialize_level1()
-    local obj_level1 = level('■')
-    luaunit.assertEquals(obj_level1.is(), 1)
+---- 同階層の比較
+function TestSection:testCompareToEquals()
+    local section1 = Section('■セクション1')
+    local section2 = Section('■セクション2')
+    luaunit.assertEquals(section1.compareTo(section2), 0)
 end
 
-----
--- 階層レベルオブジェクトの初期化のテスト(第2階層の時)
-function TestLevel:test_initialize_level2()
-    local obj_level2 = level('◆')
-    luaunit.assertEquals(obj_level2.is(), 2)
+---- 階層比較（自分が上位）
+function TestSection:testCopareToHigher()
+    local section1 = Section('■セクション1')
+    local section2 = Section('◆セクション2')
+    luaunit.assertEquals(section1.compareTo(section2), 1)
 end
 
-------
--- 階層レベルオブジェクトの初期化のテスト(第3階層の時)
-function TestLevel:test_initialize_level3()
-    local obj_level3 = level('●')
-    luaunit.assertEquals(obj_level3.is(), 3)
+---- 階層比較（自分が下位）
+function TestSection:testCompareToLower()
+    local section1 = Section('◆セクション1')
+    local section2 = Section('■セクション2')
+    luaunit.assertEquals(section1.compareTo(section2), -1)    
 end
 
-------
--- 階層レベルオブジェクトの初期化のテスト(第4階層の時)
-function TestLevel:test_initialize_level4()
-    local obj_level4 = level('▼')
-    luaunit.assertEquals(obj_level4.is(), 4)
-end
-
-------
--- 階層レベルオブジェクトの初期化のテスト(不正な値で生成しようとしたとき)
-function TestLevel:test_initialize_invalid()
-    local markInvalid = level('')
-    luaunit.assertEquals(markInvalid.is(), -1)
-end
-
-------
--- 階層レベルオブジェクトの比較のテスト、階層が低いとき
-function TestLevel:test_compareTo_lower_case()
-    local me = level('▼')
-    local other = level('●')
-    luaunit.assertEquals(me.compareTo(other), -1)
-end
-
-------
--- 階層レベルオブジェクトの比較テスト、同レベル階層のとき
-function TestLevel:test_compareTo_same_case()
-    local me = level('▼')
-    local other = level('▼')
-    luaunit.assertEquals(me.compareTo(other), 0)
-end
-
-------
--- 階層レベルオブジェクトの比較テスト、階層が高いとき
-function TestLevel:test_compareTo_higher_case()
-    local me = level('●')
-    local other = level('▼')
-end
-
-
-TestRelation = {}
-
-function TestRelation:test_iterate()
-    local relation = Relation("a")
-    relation.addChild("b")
-    relation.addChild("c")
-    relation.addChild("d")
+---- 子のイテレータのテスト
+function TestSection:testIterateChildren()
+    local parent = Section('■親')
+    local child1 = Section('◆子１', parent)
+    local child2 = Section('◆子２', parent)
+    local child3 = Section('◆子３', parent)
     local result = {}
-    for child in relation.iterateChildren() do
+    for child in parent.iterateChildren() do
         table.insert(result, child)
     end
-    luaunit.assertEquals(result, {"b", "c", "d"})
+    luaunit.assertEquals(result, {child1, child2, child3})
 end
 
 os.exit(luaunit.LuaUnit.run())
